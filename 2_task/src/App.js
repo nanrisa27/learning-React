bb7c8e24e9450a56bf3674db1374a69601fee081
Minyoi87 <59666333+Minyoi87@users.noreply.github.com>
2020-03-25 13:44:03 +0200

    My_name_is

---
+++ 2_task/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Person from"./Person/Person";
+
+import './App.css';
+
+
+
+
+ const App = () => {
+   return(
+     <div>
+       <h1>Hello World</h1>
+       <p>Hello, World</p>
+       <Person/>
+     </div>
+   );
+ };
+ export default App;
---
+++ 2_task/src/Person/Person.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Person.css";
+
+
+
+const Person = () => {
+    return(
+        <div className="card">
+            <p>Hello, My name is Mimi, and iam 30years old</p>
+
+        </div>
+    );
+      
+    
+      
+    
+  };
+
+  export default Person;
