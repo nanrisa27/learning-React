b5d163fcf8997afd82654ebd528cebddcff1df57
Minyoi87 <59666333+Minyoi87@users.noreply.github.com>
2020-03-30 12:26:16 +0300

    buttons

--- 3_task/src/App.js
+++ 3_task/src/App.js
@@ -6,10 +6,33 @@ class App extends Component{
     like : 0
   };
 
+  addHander = () => {
+    console.log("test");
+    this.setState({
+      like:this.state.like + 1
+    });
+  }; 
+  IncrementItem = () => {
+    this.setState({ like: this.state.like + 1 });
+  }
+  DecreaseItem = () => {
+    this.setState({ like: this.state.like - 1 });
+  }
+  resetHandler = () => {
+    this.setState({ like: 0 });
+  };
+ 
+
   render (){
     return (
       <div>
+        <button onClick={this.IncrementItem}>Click to increase</button>
+        <button onClick={this.DecreaseItem}>Click to decrease</button>
+
         <h1>Total likes: {this.state.like}</h1>
+        <button onClick={this.addHandler}>
+        Add likes
+      </button>
       </div>
     );
   }
