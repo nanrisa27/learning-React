13abfdc09d60d5851beda48927b2edbeac85b8fc
Minyoi87 <59666333+Minyoi87@users.noreply.github.com>
2020-03-30 11:13:54 +0300

    tiotal likes

--- 3_task/src/App.js
+++ 3_task/src/App.js
@@ -1,14 +1,17 @@
 import React,{Component} from 'react';
 import './App.css';
 
-
 class App extends Component{
+  state = {
+    like : 0
+  };
+
   render (){
     return (
       <div>
-        <h1>Total likes</h1>
+        <h1>Total likes: {this.state.like}</h1>
       </div>
-    )
+    );
   }
 }
 
