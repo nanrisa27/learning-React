e8c4c1f57903f5afd712b961822d4bff3959e121
Minyoi87 <59666333+Minyoi87@users.noreply.github.com>
2020-03-30 13:12:16 +0300

    complete task 3

--- 3_task/src/App.js
+++ 3_task/src/App.js
@@ -28,8 +28,9 @@ class App extends Component{
       <div>
         <button onClick={this.IncrementItem}>Click to increase</button>
         <button onClick={this.DecreaseItem}>Click to decrease</button>
+        <button onClick={this.resetHandler}>Click to reset</button>
 
-        <h1>Total likes: {this.state.like}</h1>
+        <h1 className = {this.state.like == 0 ? "like": this.state.like % 2 === 0 ? " like even" : "like odd"} >Total likes: {this.state.like}</h1>
         <button onClick={this.addHandler}>
         Add likes
       </button>
@@ -39,6 +40,8 @@ class App extends Component{
 }
 
 export default App;
+
+
 
 
 
---
+++ 3_task/src/Header/Header.js
@@ -0,0 +1,25 @@
+import React from "react"
+
+import React from 'react'
+
+
+const Header = () => {
+  return(
+    <div>
+    <h1>Tast 3 Total Likes <Date>30.03.2020</Date></h1>
+  </div>
+      
+  );
+    
+  
+    
+  
+};
+      
+    
+      
+    
+
+
+
+
