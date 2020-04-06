f0ff335cd6e56ae85960d371c6a5817c5f2d9ed2
Minyoi87 <59666333+Minyoi87@users.noreply.github.com>
2020-04-06 13:33:13 +0300

    defining the props

--- game_1/src/App.js
+++ game_1/src/App.js
@@ -1,12 +1,38 @@
 import React, { Component } from 'react';
+import './App.css';
+import Circle from './Circle/Circle';
 
 
-class 
+
 
 class App extends Component {
+  state = {
+    score: 0,
+
+  };
+
+  clickHandler = (circleID) =>{
+    console.log ('clicked ', circleID);
+    this.setState({
+      score: this.statescore + 1,
+    });
+  };
+
+
   render() {
     return (
-      <div>
+      <div className = 'App'>
+        <h1> Speed Game</h1>
+
+    <p> Your score is : {this.state.score}</p>
+        <main>
+          <Circle click={this.clickHandler.bind(this,1)} />
+          <Circle click={this.clickHandler.bind(this,1)} />
+          <Circle click={this.clickHandler.bind(this,1)} />
+        </main>
+
+        <button className = 'btn'>Start</button>
+        <button className = 'btn '>Quit Game</button>
         
       </div>
     );
---
+++ game_1/src/Circle/Circle.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Circle.css';
+
+const Circle = props => {
+    return (
+        <div className = 'Circle'onClick= {props.click}>
+            
+        </div>
+    );
+}
+
+export default Circle;
